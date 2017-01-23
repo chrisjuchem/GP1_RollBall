187df7bdbdee64e75a4445416ad043817bee0910
chrisjuchem <chrisjuchem15@gmail.com>
2017-01-23 14:21:32 -0500

    player movement & pickup collision and mvmnt

---
+++ Assets/Scripts/Pickup.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour {
+
+	public float bobHeight;
+	public float bobDuration;
+
+	public float rotateSpeed = 1;
+
+	private float height;
+
+	void Start () {
+		height = transform.position.y;
+	}
+
+	void Update () {
+		Rotate ();
+		Bob ();
+	}
+
+	//Rotates the pickup by the rotate speed each frame around the vertical axis
+	void Rotate(){
+		transform.Rotate (new Vector3 (0, -90, 0) * Time.deltaTime * rotateSpeed);
+	}
+
+	//Bobs the pickup up and down fully once every bobDuration seconds
+	void Bob() {
+		float y = height + (Mathf.Sin (Time.time * 2 * Mathf.PI / bobDuration) * bobHeight);
+		transform.position = new Vector3 (transform.position.x, y, transform.position.z);
+	}
+
+
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	public float speed;
+
+	void FixedUpdate () {
+		Move ();
+	}
+
+	void Move() {
+		Vector3 dir = new Vector3 (Input.GetAxis ("Horizontal"), 0.0f, Input.GetAxis ("Vertical"));
+		gameObject.GetComponent<Rigidbody> ().AddForce (dir * speed);
+	}
+
+	void OnTriggerEnter (Collider other) {
+		if (other.gameObject.CompareTag("Pickup")) {
+			other.gameObject.SetActive (false);
+		}
+	}
+}
